8345981e1e3cf119c672a9aa7a58956cece26632
i-osama <zeeshan2k01@gmail.com>
2024-08-12 03:32:44 +0900

    add image file

--- src/components/Write-post/TextEditor.js
+++ src/components/Write-post/TextEditor.js
@@ -52,6 +52,7 @@ const TextEditor = () => {
       alert('You cannot add more than 2 images.');
       return;
     }
+    
     const imageUrl = prompt('Enter image URL');
     if (imageUrl) {
       setImages([...images, imageUrl]);
@@ -63,15 +64,28 @@ const TextEditor = () => {
     setImages(newImages);
   };
 
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file && images.length < 2) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImages([...images, reader.result]);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      alert('You cannot add more than 2 images.');
+    }
+  };
+
   return (
     <div className="container mt-5">
       <div className="d-flex justify-content-between mb-3">
         <div>
-          <button className="btn btn-outline-secondary mr-2" onClick={handleUndo}><FaUndo /></button>
+          <button className="btn btn-outline-secondary mx-2" onClick={handleUndo}><FaUndo /></button>
           <button className="btn btn-outline-secondary mr-2" onClick={handleRedo}><FaRedo /></button>
           <span className="text-right mx-3">
             <b> Total Words: {wordCount} | Total Characters: {charCount} </b>
-      </span>
+          </span>
           <input
             type="number"
             className="form-control d-inline-block mr-2"
@@ -82,11 +96,18 @@ const TextEditor = () => {
             max={48}
             step={1}
           />
-          <button className="btn btn-outline-secondary" onClick={handleImageAdd}>Image</button>
+          <button className="btn btn-outline-secondary mx-2" onClick={handleImageAdd}>Image URL</button>
+          <input
+            type="file"
+            className="form-control d-inline-block"
+            style={{ width: '200px' }}
+            accept="image/*"
+            onChange={handleFileChange}
+          />
         </div>
         <div>
           <button className="btn btn-secondary mr-2" onClick={handleSave}>Save</button>
-          <button className="btn btn-primary" onClick={handlePublish}>Publish</button>
+          <button className="btn btn-primary mx-2" onClick={handlePublish}>Publish</button>
         </div>
       </div>
       <input
@@ -96,38 +117,13 @@ const TextEditor = () => {
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
-      <ReactQuill
-        ref={quillRef}
-        value={content}
-        onChange={handleContentChange}
-        modules={{
-          toolbar: [
-            [{ 'font': [] }],
-            [{ 'size': ['small', false, 'large', 'huge'] }],
-            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-            [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-            ['link', 'image'],
-            ['clean']
-          ],
-          history: {
-            delay: 500,
-            maxStack: 100,
-            userOnly: true
-          }
-        }}
-        formats={[
-          'font', 'size',
-          'bold', 'italic', 'underline', 'strike', 'blockquote',
-          'list', 'bullet', 'indent',
-          'link', 'image'
-        ]}
-        placeholder="Write your article here..."
-        style={{ fontSize: `${fontSize}px` }}
-      />
+
       <div className="mt-3">
         {images.map((image, index) => (
           <div key={index} className="position-relative mb-3">
-            <img src={image} alt="Added" className="img-fluid" style={{ width: '100%', maxHeight: '300px' }} />
+            <div className="d-flex justify-content-around mt-4 mx-2">
+              <img src={image} alt="Added" className="img-fluid" style={{ width: 'auto', maxHeight: '300px' }} />
+            </div>
             <button
               className="btn btn-danger position-absolute"
               style={{ top: '10px', right: '10px' }}
@@ -138,7 +134,37 @@ const TextEditor = () => {
           </div>
         ))}
       </div>
-   
+
+      <div className="mb-5">
+        <ReactQuill
+          ref={quillRef}
+          value={content}
+          onChange={handleContentChange}
+          modules={{
+            toolbar: [
+              [{ 'font': [] }],
+              [{ 'size': ['small', false, 'large', 'huge'] }],
+              ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+              [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+              ['link'],
+              ['clean']
+            ],
+            history: {
+              delay: 500,
+              maxStack: 100,
+              userOnly: true
+            }
+          }}
+          formats={[
+            'font', 'size',
+            'bold', 'italic', 'underline', 'strike', 'blockquote',
+            'list', 'bullet', 'indent',
+            'link'
+          ]}
+          placeholder="Write your article here..."
+          style={{ fontSize: `${fontSize}px` }}
+        />
+      </div>
     </div>
   );
 };
