1d1465ce0c5871f30819ce08c135bf93d421759b
i-osama <zeeshan2k01@gmail.com>
2024-08-12 13:06:26 +0900

    login signup button added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
         <div>
             <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/admin-profile"> <BsPersonCircle/> </Link>
+                    <Link className="navbar-brand" to="/admin-profile"> <BsPersonCircle /> </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -54,10 +54,17 @@ const Navbar = () => {
                             </li> */}
 
                         </ul>
-                        <form className="d-flex" role="search">
+
+                        {!localStorage.getItem('token') ? <form className="d-flex" role="search">
+                            <Link className="btn btn-outline-light mx-1" to="/login" role="button">Login</Link>
+                            <Link className="btn btn-outline-light mx-1" to="/signup" role="button">Signup</Link>
+                        </form> : <form className="d-flex" role="search">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-outline-light" type="submit">Search</button>
                         </form>
+                        }
+
+
                     </div>
                 </div>
             </nav>
